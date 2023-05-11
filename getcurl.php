5f0b4ab8af588b9ae42ab03d74cef67cb449b437
Adela Setiabudi P <117787133+Setiabudi05@users.noreply.github.com>
2023-05-11 18:32:45 +0700

    Add files via upload

---
+++ getcurl.php
@@ -0,0 +1,37 @@
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://farmasi.mimoapps.xyz/mimoqss2auyqD1EAlkgZCOhiffSsFl6QqAEIGtM',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURLOPT_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+));
+
+$response = curl_exec($curl);
+curl_close($curl);
+$reponse_array = json_decode($reponse,true);
+
+$onscreen = '<table class="table" width="100%">
+              $json = json_decode($response,true);
+
+$filterdata = array();
+foreach($json as $data){
+    $code = $data['i_code'];
+    $name = $data['i_name'];
+    $grup = $data['g_code'];
+    $harga = $data['i_sell'];
+    $kuantiti = $data['_qty'];
+    if (substr($grup , 0,1)=="B"){
+    echo "Kode Barang = ".$code."<br>";
+    echo "Nama Barang = ".$name."<br>";
+    echo "Grup Barang = ".$grup."<br>";
+    echo "Harga Jual = ".$harga."<br>";
+    echo "Quantity = ".$kuantiti."<br>";
+ }      
+ $onscreen.='</table>';
+ echo $onscreen;      
+?>
